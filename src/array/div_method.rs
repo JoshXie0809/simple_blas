3db15cb5c6d0cb3c1414e37ea35a15cfdeb730ca
JoshXie <61863381+JoshXie0809@users.noreply.github.com>
2024-08-10 21:11:27 +0800

    divide method

--- src/array/div_method.rs
+++ src/array/div_method.rs
@@ -20,6 +20,30 @@ where T: Add<Output=T> + Mul<Output=T> + Div<Output=T>
                     }
                     *x *= *y
                 },
+                (Self::Array1D { arr: arr1 }, 
+                 Self::Array1D { arr: arr2 }) => {
+                    let len1 = arr1.len();
+                    if len1 != arr2.len() {
+                        return Err(ListError::DifferentLength1D);
+                    }
+    
+                    for i in 0..(len1) {
+                        arr1[i] = arr1[i] / arr2[i];
+                    }
+                },
+        
+                (Self::Array1D { arr: arr1 }, 
+                 Self::Scalar(val)) => {
+                    
+                    if *val == T::default() {
+                        return Err(ListError::DivisionByZero);
+                    }
+                    
+                    let len1 = arr1.len();
+                    for i in 0..(len1) {
+                        arr1[i] = arr1[i] / *val;
+                    }
+                },
             
             _ => return Err(ListError::MismatchedTypes),
         }
--- tests/div_method_tests.rs
+++ tests/div_method_tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use simple_blas::array::Array;
+    use simple_blas::array::{Array, ListError};
     // use simple_blas::array::ListError;
 
     #[test]
@@ -17,5 +17,34 @@ mod tests {
         };
     }
 
+    #[test]
+    fn div_method_array_1d() -> Result<(), ListError>{
+
+        // arr_1d devide arr_1d
+        let mut arr_1d = Array::new_array_1d(
+            Box::new([1.1, 1.2, 1.3])
+        );
+
+        let other_arr_1d = Array::new_array_1d(
+            Box::new([2.2, 2.4, 2.6])
+        );
+
+        arr_1d.div(&other_arr_1d)?;
+        assert_eq!(arr_1d, Array::Array1D { arr: Box::new([0.5, 0.5, 0.5]) });
+
+
+        // arr_1d devide scalar
+        let scalar = Array::Scalar(0.5);
+        arr_1d.div(&scalar)?;
+        assert_eq!(arr_1d, Array::Array1D { arr: Box::new([1.0, 1.0, 1.0]) });
+
+        // test divide by zero
+        let scalar = Array::Scalar(0.0);
+        if let Err(err) = arr_1d.div(&scalar) {
+            assert_eq!(err, ListError::DivisionByZero)
+        }
+
+        Ok(())
+    }
 
 }
