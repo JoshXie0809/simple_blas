8ef1a7aafbf59583f6d502b75b6fd7bf87d82484
JoshXie <61863381+JoshXie0809@users.noreply.github.com>
2024-08-18 02:13:25 +0800

    inverse

--- tests/inv_method_tests.rs
+++ tests/inv_method_tests.rs
@@ -28,4 +28,29 @@ pub mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn inv_arr_2d_2() -> Result<(), ListError>{
+        let mut arr = Array::new_array_2d(
+            Box::new([1.0, 22.0, 5.0,
+                           3.0, 4.0, 11.0,
+                           12.0, 14.0, 19.0]), 
+            (3, 3), 
+            true
+        )?;
+
+        let arr2 = Array::new_array_2d(
+            Box::new([1.0, 22.0, 5.0,
+                           3.0, 4.0, 11.0,
+                           12.0, 14.0, 19.0]), 
+            (3, 3), 
+            true
+        )?;
+
+        arr.inv()?;
+        arr.mmult(&arr2)?;
+
+        println!("{:?}", arr);
+        Ok(())
+    }
 }
